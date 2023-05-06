dcbf38475cc1a5670eb9e1da311bea3eef7fb109
BenJosiah <benjosiah90@gmail.com>
2023-05-06 15:29:33 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,271 @@
+<template>
+<div>
+  <section class="bg-purple-900 h-screen w-full text-center align-middle flex text-white justify-center items-center -mt-20 px-10">
+
+    <div class="w-full">
+        <span class="text-3xl" >
+          Lorem ipsum dolor sit amet consectetur.
+        </span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. maxime ea, magni minima asperiores a et repudiandae. Explicabo expedita aut reprehenderit? Accusantium, est unde.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi harum nemo alias ullam assumenda excepturi explicabo eligendi deleniti numquam corporis eos dignissimos neque, dolor impedit eius totam eum maiores ab.
+        </p>
+
+        <div class="mt-4 flex px-20 justify-center">
+          <button class="mx-10 px-10 py-5 border-2 rounded-full border-yellow-600 text-yellow-600">
+              Quick start
+          </button>
+
+          <button class="mx-10 px-10 py-5 border-2 rounded-full border-white bg-yellow-600">
+            Learn more
+          </button>
+        </div>
+    </div>
+
+
+
+
+  </section>
+
+  <section class="w-full bg-gray-100 px-10 py-10 justify-around">
+    <div class="m-auto text-center text-2xl font-semibold py-5 ">
+      Lorem ipsum dolor sit amet, consectetur adipisicing.
+    </div>
+
+    <div class="w-full flex">
+      <div class="h-96 w-1/2 bg-black mx-2">
+
+      </div>
+      <div class="w-1/2 align-middle pt-20 items-center text-gray-700 px-20 text-lg">
+        <h3 class="text-2xl my-4 font-semibold ">
+          Courses we offer
+        </h3>
+        <div class="w-full my-3 ">
+          <span></span> 
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.
+        </div>
+
+        <div class="w-full my-3">
+          <span></span> 
+          Lorem ipsum dolor sit amet. Lorem, ipsum dolor.
+        </div>
+
+        <div class="w-full my-3">
+          <span></span> 
+          <span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur.</span>
+        </div>
+
+        <div class="w-full my-3">
+          <span></span> 
+          <span>Lorem ipsum dolor sit amet. Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="w-full bg-white px-10 py-10 items-center justify-around h-auto">
+    <div class="m-auto text-center text-2xl font-semibold py-5 ">
+      Lorem ipsum dolor sit amet, consectetur adipisicing.
+    </div>
+    <div class="w-full flex">
+      <div class="w-1/2 align-middle pt-20 items-center text-gray-700 px-20 text-lg">
+        <h3 class="text-2xl my-4 font-semibold ">
+          Courses we offer
+        </h3>
+        <div class="w-full my-3 ">
+          <span></span> 
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.
+        </div>
+
+        <div class="w-full my-3">
+          <span></span> 
+          Lorem ipsum dolor sit amet. Lorem, ipsum dolor.
+        </div>
+
+        <div class="w-full my-3">
+          <span></span> 
+          <span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur.</span>
+        </div>
+
+        <div class="w-full my-3">
+          <span></span> 
+          <span>Lorem ipsum dolor sit amet. Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+        </div>
+
+      </div>
+      <div class="h-96 w-1/2 bg-black mx-2">
+
+      </div>
+    </div>
+  </section>
+
+  <section class="border-y border-1 border-gray-300 w-full py-10 px-5 my-2">
+    <div class="m-auto text-center text-2xl font-semibold py-5 ">
+      Lorem ipsum dolor sit amet, consectetur adipisicing.
+    </div>
+    <div class="flex justify-evenly items-center">
+
+      <div class="text-center"> 
+        <div class="bg-black h-32 w-32 m-auto rounded mb-2">
+
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet.
+        </div>
+      </div>
+      
+      <div class="text-center">
+        <div class="bg-black h-32 w-32 m-auto rounded mb-2">
+
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet.
+        </div>
+      </div>
+
+      <div class="text-center">
+        <div class="bg-black h-32 w-32 m-auto rounded mb-2">
+
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet.
+        </div>
+      </div>
+
+      <div class="text-center">
+        <div class="bg-black h-32 w-32 m-auto rounded mb-2">
+
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet.
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <section class="w-full"> 
+    <div class="m-auto text-center text-2xl font-semibold py-5 ">
+      Lorem ipsum dolor sit amet, consectetur adipisicing.
+    </div>
+
+    <div class="w-2/3 m-auto flex flex-wrap justify-evenly items-center">
+      <div class="w-96 px-3 pt-24 py-3 text-center border border-gray-900 relative my-10">
+            <span class="absolute bg-black w-20 h-20 rounded-full top-1 left-0 right-0 m-auto  ">
+
+            </span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum est inventore ex architecto! Eos iure ullam, possimus facere reprehenderit atque aperiam eligendi.
+          </span>
+      </div>
+
+      <div class="w-96 px-3 pt-24 py-3 text-center border border-gray-900 relative">
+            <span class="absolute bg-black w-20 h-20 rounded-full top-1 left-0 right-0 m-auto  ">
+
+            </span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum est inventore ex architecto! Eos iure ullam, possimus facere reprehenderit atque aperiam eligendi.
+          </span>
+      </div>
+
+      <div class="w-96 px-3 pt-24 py-3 text-center border border-gray-900 relative">
+            <span class="absolute bg-black w-20 h-20 rounded-full top-1 left-0 right-0 m-auto  ">
+
+            </span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum est inventore ex architecto! Eos iure ullam, possimus facere reprehenderit atque aperiam eligendi.
+          </span>
+      </div>
+
+      <div class="w-96 px-3 pt-24 py-3 text-center border border-gray-900 relative">
+            <span class="absolute bg-black w-20 h-20 rounded-full top-1 left-0 right-0 m-auto  ">
+
+            </span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum est inventore ex architecto! Eos iure ullam, possimus facere reprehenderit atque aperiam eligendi.
+          </span>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="bg-black text-white px-20 pt-10 pb-10">
+    <div class="flex flex-wrap px-20 justify-between">
+      <div class="w-2/5 m-1">
+          <h2>
+            Lorem, ipsum.
+          </h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, fuga!
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate nostrum incidunt nobis earum praesentium quaerat sequi aut minima? Eius, nemo!
+          </p>
+      </div>
+      <div class="w-1/5 m-1">
+          <ul>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+          </ul>
+      </div>
+      <div class="w-1/5 m-1">
+
+        <ul>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+            <li>
+              Lorem, ipsum dolor.
+            </li>
+          </ul>
+
+      </div>
+
+    </div>
+  </section>
+
+  
+  
+</div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
